7cdc74ad6777b8e7774db571692ea3b736764e8c
Hernan Case <her.case@live.com.ar>
2020-08-02 19:37:49 -0300

    Add elements to LocalStorage

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const formInput = document.querySelector('.content__form__input');
 const itemList = document.querySelector('.content__list').children[0];
 
 // EventListeners
-form.addEventListener('submit', addItemToList);
-form.addEventListener('onkeyup', addItemToList);
-itemList.addEventListener('click', removeItemFromList);
+
+eventsListeners();
+
+function eventsListeners(){
+  form.addEventListener('submit', addItemToList);
+  form.addEventListener('onkeyup', addItemToList);
+  itemList.addEventListener('click', removeItemFromList);
+  document.addEventListener('DOMContentLoaded', displayItemsFromLocalStorage);
+}
 
 // Functions
 
@@ -22,6 +28,7 @@ function addItemToList(e) {
   if (formInput.value != ""){
     newItem.innerText = formInput.value;
     itemList.appendChild(newItem);
+    addItemToLocalStorage(formInput.value);
   }
 
   newDeleteButton.className = 'content__list__delete';
@@ -39,4 +46,41 @@ function removeItemFromList(e) {
     let confirm = window.confirm('Are you sure to delete this item from the list?');
     if( confirm == true) { e.target.parentElement.remove() };
   }  
+}
+
+function addItemToLocalStorage(item){
+  let currentItems = getItemsFromLocalStorage();
+  currentItems.push(item);
+  localStorage.setItem('list-items', JSON.stringify(currentItems));
+}
+
+function getItemsFromLocalStorage(){
+  let itemsLocalStorage = localStorage.getItem("list-items");
+  let itemsList = [];
+
+  if (itemsLocalStorage != null) {
+    itemsList = JSON.parse(itemsLocalStorage);
+  };
+
+  return itemsList;
+}
+
+function displayItemsFromLocalStorage(){
+  let currentItems = getItemsFromLocalStorage();
+  
+  currentItems.forEach( item => {
+
+    //Creating list item and delete button.
+    let newItem = document.createElement('li');
+    let newDeleteButton = document.createElement('button');
+
+    newItem.className = 'content__list__item';
+
+    newItem.innerText = item;
+    itemList.appendChild(newItem);
+
+    newDeleteButton.className = 'content__list__delete';
+    newDeleteButton.innerText = 'X';
+    newItem.appendChild(newDeleteButton);
+  })
 }
